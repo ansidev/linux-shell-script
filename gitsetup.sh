b2c6b259439dfe54917181a707b4fb6fa2ecb901
ansidev <ansidev@gmail.com>
2014-02-21 13:51:25 +0700

    Fix and update functions.

--- gitsetup.sh
+++ gitsetup.sh
@@ -2,14 +2,22 @@
 ################################################################################
 # Author: ansidev                                                              #
 # Email: ansidev@gmail.com                                                     #
-# Homepage: ansidev.uni.me                                                     #
+# Homepage: http://ansidev.esy.es/blog                                         #
 # Filename: gitsetup.sh                                                        #
 # Date Created: 20/02/2014                                                     #
 # Last Updated: 20/02/2014                                                     #
-# Version: 14.02.20.0001                                                       #
+# Version: 14.02.21                                                            #
 # Description: Linux Shell script for install & setting git version control    #
+# Usage: gitsetup --help for full help                                         #
 ################################################################################
 #set -x
+#Variables
+author=ansidev
+email=ansidev@gmail.com
+homepage=http://ansidev.esy.es/blog
+version=14.02.21  
+#End Variables
+#Functions
 function gitsetup()
 {
 	echo "Installing package git. Please wait..."
@@ -19,39 +27,107 @@ function gitsetup()
 function gitusername()
 {
 	#Setup username account
-	echo "Enter your username. (Leave blank if you do not want to change current this setting)"
-	read -p "Username = " username
-	if [ "$username" != "" ]; then
-	git config --global user.name "$username"
+	if [ "$#" != "0" ]; then
+		git config --global user.name "$1"
+	else
+		echo "Enter your username. (Leave blank if you do not want to change current this setting)"
+		read -p "Username = " username
+		if [ "$username" != "" ]; then
+			git config --global user.name "$username"
+		fi
 	fi
 }
 
 function gitemail()
 {
 	#Setup email account
-	echo "Enter your email. (Leave blank if you do not want to change current this setting)"
-	read -p "Email = " email
-	if [ "$email" != "" ]; then
-	git config --global user.email "$email"
+	if [ "$#" != "0" ]; then
+		git config --global user.email "$1"
+	else
+		echo "Enter your email. (Leave blank if you do not want to change current this setting)"
+		read -p "Email = " email
+		if [ "$email" != "" ]; then
+		git config --global user.email "$email"
+		fi
 	fi
 }
+
 function gittimeout()
 {
 	#Setup timeout for password caching
-	echo "Default timeout for password caching is 15 minutes."
-	read -p "Do you want to change timeout for password caching? (y/n) " confirm
-	if [ "$confirm" == y ]; then
-		read -p "Enter timeout in second(s): " timeout
-		git config --global credential.helper 'cache --timeout=3600'
+	if [ "$#" != "0" ]; then
+		if [ "$#" == "1" ]; then
+			git config --global credential.helper "cache --timeout=$1"
+		elif [ "$#" == "3" ]; then
+			timeout=$(($1*3600+$2*60+$3))
+			git config --global credential.helper "cache --timeout=$timeout"
+		fi
+	else
+		echo "Default timeout for password caching is 15 minutes."
+		read -p "Do you want to change timeout for password caching? (y/n) " confirm
+		if [ "$confirm" == y ]; then
+			read -p "Enter timeout in second(s): " timeout
+			git config --global credential.helper 'cache --timeout=$timeout'
+		fi
 	fi
 }
 
 function gitconfig()
 {
-	gitusername
-	gitemail
-	gittimeout
+	if [ "$#" != "0" ]; then
+		gitusername $1
+		gitemail $2
+		gittimeout $3 $4 $5
+	else
+		gitusername
+		gitemail
+		gittimeout
+	fi
 }
+
+function gitinfo()
+{
+	echo "################################################################################"
+	echo "# Author: $author                                                              #"
+	echo "# Email: $email                                                     #"
+	echo "# Homepage: $homepage                                         #"
+	echo "# Version: $version                                                            #"
+	echo "# Description: Linux Shell script for install & setting git version control    #"
+	echo "################################################################################"
+	printf "\n"
+}
+
+function githelp()
+{
+	gitinfo
+	printf "%s\n%s\n%s" "Usage: gitsetup.sh [OPTION] <args>." "You can use no option or argument. If not, you should use only one option." "Arguments depend on option."
+	printf "\n%s\n" "[OPTION]"
+	printf "%s%s\t\t%s\n" " " "-a | --auto" "Default option if you use no option. [-a] = [-i] && [-c]"
+	printf "%s%s\t\t%s\n" " " "-i | --install" "Install package git."
+	printf "%s%s\t\t%s\n" " " "-c | --config" "Default config mode: username, email, timeout for password caching."
+	printf "%s\n" "Arguments for [-c | --config] option"
+	printf "%s%s\t\t%s\n\t\t\t%s\n" "   " "<args1>" "Must be git username." "Config git username."
+	printf "%s%s\t\t%s\n\t\t\t%s\n" "   " "<args2>" "Must be git email." "Config git email."
+	printf "%s%s\t\t%s\n\t\t\t%s\n" "   " "<args3> " "If there are no argument(s) after, it will be timeout, else it will be timeout hour(s)." "Config git timeout for password caching."
+	printf "%s%s\t\t%s\n" "   " "<args4>" "If exist, it will be timeout minute(s)."
+	printf "%s%s\t\t%s\n" "   " "<args5>" "If exist, it will be timeout second(s)."
+	printf "%s\n" "Example for config timeout"
+	printf "%s%s\t%s\n" "  " "gitsetup.sh -c example example@example.com 3600" "Timeout is 3600 seconds."
+	printf "%s%s\t%s\n" "  " "gitsetup.sh -c example example@example.com 1 0 0" "Timeout is 3600 seconds. (1*3600 +0*60+ 0 = 3600)"
+	printf "%s%s\t\t%s\n" " " "-u | --user" "Config only git username."
+	printf "%s%s\t\t%s\n" " " "-e | --email" "Config only git email."
+	printf "%s%s\t\t%s\n" " " "-t | --timeout" "Config only git timeout for password caching. See <arg3|args4|args5> in 'Arguments for [-c | --config] option' for more information."
+	printf "%s%s\t%s\n" " " "-h | -? | --help" "Help for gitsetup."
+	printf "%s%s\t\t%s\n" " " "-I | --info" "Information about gitsetup shell script."
+	printf "\n"
+}
+
+function gitfinish()
+{
+	echo "Finished setup!"
+	exit
+}
+#End Functions
 
 if [ "$#" == "0" ]; then
 	gitsetup
@@ -59,27 +135,40 @@ if [ "$#" == "0" ]; then
 fi
 while (( "$#" )); do
 case $1 in
-	"-i" | "--install" )
+	"-i" | "--install")
 		gitsetup
+		gitfinish
 		;;
 	"-c" | "--config")
-		gitconfig
+		gitconfig $2 $3 $4 $5 $6
+		gitfinish
 		;;
 	"-u" | "--user")
-		gitusername
+		gitusername $2
+		gitfinish
 		;;
 	"-e" | "--email")
-		gitemail
+		gitemail $2
+		gitfinish
 		;;
 	"-t" | "--timeout")
-		gittimeout
+		gittimeout $2 $3 $4
+		gitfinish
 		;;
-	*)
+	"-h" | "-?" | "--help")
+		githelp
+		;;
+	"-I" | "--info")
+		gitinfo
+		;;
+	"" | "-a" | "--auto")
 		gitsetup
 		gitconfig
+		gitfinish
+		;;
+	*)
+		githelp
 		;;
 esac
 shift
 done
-echo "Finished setup!"
-
